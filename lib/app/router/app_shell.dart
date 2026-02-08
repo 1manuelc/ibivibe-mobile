835111dedcc69c29641375289816fc70b82c3cb3
Manuel C. <manuelwn21@gmail.com>
2026-02-07 22:53:28 -0300

    feat: implements app_shell for app navigation and exit confirmation

--- lib/app/router/app_shell.dart
+++ lib/app/router/app_shell.dart
@@ -1,11 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:ibiapabaapp/shared/ui/exit_app_dialog.dart';
 import 'package:ibiapabaapp/shared/ui/navbar.dart';
 
-class AppShell extends StatelessWidget {
+class AppShell extends StatefulWidget {
   final Widget child;
   const AppShell({super.key, required this.child});
+
+  @override
+  State<AppShell> createState() => _AppShellState();
+}
+
+class _AppShellState extends State<AppShell> {
+  DateTime? _lastBackPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -15,15 +23,39 @@ class AppShell extends StatelessWidget {
     return PopScope(
       canPop: !locationIsHome,
       onPopInvokedWithResult: (didPop, _) {
-        if (locationIsHome && !didPop) {
-          showExitAppDialog(context);
+        if (!locationIsHome || didPop) return;
+
+        final now = DateTime.now();
+
+        if (_lastBackPressed == null ||
+            now.difference(_lastBackPressed!) > const Duration(seconds: 2)) {
+          _lastBackPressed = now;
+
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(
+                behavior: .fixed,
+                duration: const Duration(seconds: 2),
+                elevation: 10,
+                shape: Border.all(color: Colors.grey.shade800),
+                backgroundColor: Colors.black,
+                content: Text(
+                  textAlign: .center,
+                  'Volte novamente para sair',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ),
+            );
+        } else {
+          exit(0);
         }
       },
       child: Scaffold(
         body: SafeArea(
           child: Column(
             children: [
-              Expanded(child: child),
+              Expanded(child: widget.child),
               const Navbar(),
             ],
           ),
