1a2253ad1e5dacdf0f00fdcb5bd2451a253255e6
Manuel C. <manuelwn21@gmail.com>
2026-02-17 11:35:19 -0300

    refactor(tappable_container): use FTappable

---
+++ lib/shared/ui/tappable_container.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:forui/forui.dart'; // Importação necessária
+
+class TappableContainer extends StatelessWidget {
+  final Widget child;
+  final VoidCallback? onTap;
+  final BorderRadius borderRadius;
+  final Color color;
+
+  const TappableContainer({
+    super.key,
+    required this.child,
+    required this.borderRadius,
+    required this.color,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FTappable(
+      onPress: onTap,
+      child: Container(
+        decoration: BoxDecoration(color: color, borderRadius: borderRadius),
+        child: child,
+      ),
+    );
+  }
+}
