1b97ff4469f3e7bb69c01256aa1dd892c878bb94
Manuel C. <manuelwn21@gmail.com>
2026-02-17 11:58:49 -0300

    refactor(SponsoredHighlights): implements carousel instead of story_view

--- lib/features/home/presentation/widgets/sponsored_highlights.dart
+++ lib/features/home/presentation/widgets/sponsored_highlights.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:forui/forui.dart';
-import 'package:story_view/story_view.dart';
 
 class SponsoredHighlights extends StatefulWidget {
   const SponsoredHighlights({super.key});
@@ -10,94 +11,110 @@ class SponsoredHighlights extends StatefulWidget {
 }
 
 class _SponsoredHighlightsState extends State<SponsoredHighlights> {
-  final StoryController _controller = StoryController();
+  int activeIndex = 0;
+  final CarouselSliderController _carouselController =
+      CarouselSliderController();
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+  final List<String> _items = [
+    "Destaques patrocinados",
+    "Promoções imperdíveis",
+    "Ofertas próximas de você",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 1 / 1,
       child: ClipRRect(
-        borderRadius: .circular(16),
+        borderRadius: BorderRadius.circular(16),
         child: Stack(
           children: [
-            GestureDetector(
-              behavior: HitTestBehavior.translucent,
-              onLongPressStart: (_) => _controller.pause(),
-              onLongPressEnd: (_) => _controller.play(),
-              child: MouseRegion(
-                onEnter: (_) => _controller.pause(),
-                onExit: (_) => _controller.play(),
-                child: StoryView(
-                  controller: _controller,
-                  repeat: true,
-                  inline: true,
-                  indicatorForegroundColor:
-                      context.theme.colors.mutedForeground,
-                  indicatorColor: Colors.black26,
-                  progressPosition: ProgressPosition.bottom,
-                  indicatorOuterPadding: const EdgeInsets.symmetric(
-                    horizontal: 12,
-                    vertical: 12,
-                  ),
-                  storyItems: [
-                    _bannerItem("Destaques patrocinados", context),
-                    _bannerItem("Promoções imperdíveis", context),
-                    _bannerItem("Ofertas próximas de você", context),
-                  ],
-                ),
+            CarouselSlider.builder(
+              carouselController: _carouselController,
+              itemCount: _items.length,
+              itemBuilder: (context, index, realIndex) {
+                return _bannerItem(context);
+              },
+              options: CarouselOptions(
+                height: double.infinity,
+                viewportFraction: 1,
+                autoPlay: true,
+                pageSnapping: true,
+                pauseAutoPlayOnTouch: true,
+                enableInfiniteScroll: false,
+                autoPlayInterval: const Duration(seconds: 4),
+                onPageChanged: (index, reason) {
+                  setState(() => activeIndex = index);
+                },
               ),
             ),
 
-            Positioned(top: 12, left: 12, child: _SponsoredLabel()),
+            // Positioned(top: 12, left: 12, child: _SponsoredLabel()),
+            Positioned(
+              bottom: 12,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: AnimatedSmoothIndicator(
+                  activeIndex: activeIndex,
+                  count: _items.length,
+                  effect: ExpandingDotsEffect(
+                    dotHeight: 8,
+                    dotWidth: 8,
+                    activeDotColor: context.theme.colors.foreground,
+                    dotColor: context.theme.colors.foreground.withAlpha(164),
+                    expansionFactor: 3,
+                    spacing: 8,
+                  ),
+                  onDotClicked: (index) =>
+                      _carouselController.animateToPage(index),
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 
-  StoryItem _bannerItem(String text, BuildContext context) {
-    return StoryItem(
-      Container(
-        color: context.theme.colors.muted,
-        alignment: Alignment.center,
-        child: Text(
-          text,
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w500,
-            color: context.theme.colors.foreground,
-          ),
-        ),
-      ),
-      duration: const Duration(seconds: 4),
+  Widget _bannerItem(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: context.theme.colors.muted,
+      alignment: Alignment.center,
+      child: Image.asset('assets/images/banner-trilhao-quadrado.webp'),
     );
   }
 }
 
-class _SponsoredLabel extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-      decoration: BoxDecoration(
-        color: context.theme.colors.primary,
-        borderRadius: BorderRadius.circular(6),
-      ),
-      child: Row(
-        children: [
-          Icon(
-            Icons.campaign,
-            size: 14,
-            color: context.theme.colors.primaryForeground,
-          ),
-        ],
-      ),
-    );
-  }
-}
+// class _SponsoredLabel extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+//       decoration: BoxDecoration(
+//         color: context.theme.colors.primary,
+//         borderRadius: BorderRadius.circular(6),
+//       ),
+//       child: Row(
+//         mainAxisAlignment: .center,
+//         crossAxisAlignment: .center,
+//         children: [
+//           Icon(
+//             Icons.campaign,
+//             size: 24,
+//             color: context.theme.colors.primaryForeground,
+//           ),
+//           Text(
+//             '2° Trilhão Tianguá Offroad',
+//             style: TextStyle(
+//               fontWeight: .w600,
+//               fontSize: 16,
+//               color: context.theme.colors.primaryForeground,
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
