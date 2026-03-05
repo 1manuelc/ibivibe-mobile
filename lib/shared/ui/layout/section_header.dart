09a758731cc885dfeb04252149b447520f43d7ce
Manuel C. <manuelwn21@gmail.com>
2026-03-05 15:12:12 -0300

    feat(SectionHeader): implements SectionHeader for standartize ui

---
+++ lib/shared/ui/layout/section_header.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:forui/forui.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+class SectionHeader extends StatelessWidget {
+  final String title;
+  final Function? onSeeAllTap;
+  const SectionHeader({
+    super.key,
+    required this.title,
+    required this.onSeeAllTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 12,
+      crossAxisAlignment: .center,
+      children: [
+        Skeleton.replace(
+          replace: true,
+          replacement: Bone.text(fontSize: 18, textAlign: .start, width: 188),
+          child: Text(
+            title,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            strutStyle: StrutStyle(leading: 1.2),
+          ),
+        ),
+
+        if (onSeeAllTap != null)
+          FButton.raw(
+            onPress: () => onSeeAllTap?.call(),
+            style: FButtonStyle.ghost(),
+            child: Text(
+              'Ver tudo',
+              style: context.theme.typography.sm.copyWith(
+                fontWeight: .w500,
+                color: context.theme.colors.mutedForeground,
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+}
