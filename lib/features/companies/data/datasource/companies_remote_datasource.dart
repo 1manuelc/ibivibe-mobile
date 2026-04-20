63d9d5e413336121077cbae77206a5090518853e
Manuel C. <manuelwn21@gmail.com>
2026-04-20 12:50:09 -0300

    refactor: remove companies feature and related components
    - Added businesses section widget to replace companies section.

--- lib/features/companies/data/datasource/companies_remote_datasource.dart
+++
@@ -1,6 +0,0 @@
-import 'package:ibiapabaapp/features/companies/domain/entities/company.dart';
-
-abstract class CompaniesRemoteDatasource {
-  Future<List<Company>> getAllCompanies();
-  Future<Company> getCompanyById(String id);
-}
--- lib/features/companies/domain/repositories/companies_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:ibiapabaapp/core/errors/failures/failures.dart';
-import 'package:ibiapabaapp/features/companies/domain/entities/company.dart';
-
-abstract class CompaniesRepository {
-  Future<Either<AppFailure, List<Company>>> getAllCompanies();
-  Future<Either<AppFailure, Company?>> getCompanyById(String id);
-}
