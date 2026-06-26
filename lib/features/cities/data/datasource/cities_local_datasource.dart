885603c640106068f5df1a2d78593af43f09ab05
Manuel C. <1manuelc.dev@gmail.com>
2026-06-26 19:53:51 -0300

    refactor(cities): replace Freezed with Equatable, remove use cases, simplify repo

--- lib/features/cities/data/datasource/cities_local_datasource.dart
+++
@@ -1,8 +0,0 @@
-import 'package:ibiapabaapp/shared/models/city.dart';
-
-abstract class CitiesLocalDatasource {
-  Future<List<City>> getCachedCities();
-  Future<City?> getCityById(String id);
-  Future<void> cacheCities(List<City> cities);
-  Future<void> clearCache();
-}
--- lib/features/cities/data/datasource/cities_remote_datasource.dart
+++
@@ -1,5 +0,0 @@
-import 'package:ibiapabaapp/shared/models/city.dart';
-
-abstract class CitiesRemoteDatasource {
-  Future<List<City>> getAllCities();
-}
